704ee0b6744f0643dd2faba40dc8b036af7c635c
alen66 <centr-opt-pavl@yandex.ru>
2020-06-27 19:25:27 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/pin.js
+++ js/pin.js
@@ -1,26 +1,36 @@
 'use strict';
 
-(function map() {
+(function pin() {
   var mapElement = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
 
-  function makeElement(arr) {
+  function makeElement(arr, i) {
 
     var pinTemplateElement = document.querySelector('#pin').content;
     var pinButtonElement = pinTemplateElement.querySelector('.map__pin');
     var pinElement = pinButtonElement.cloneNode(true);
-
     pinElement.querySelector('img').src = arr.author.avatar;
     pinElement.querySelector('img').alt = arr.offer.title;
+    pinElement.querySelector('img').setAttribute('data-index', i);
     pinElement.style = 'left:' + (arr.location.x + window.data.X) + 'px; top: ' + (arr.location.y + window.data.Y) + 'px;';
-
     return pinElement;
   }
 
-  window.data.resultGen.forEach(function (el) {
-    fragment.appendChild(makeElement(el));
-  });
+  var resultXHR = {};
+  function successHandler(author) {
+    author.forEach(function (el, index) {
+      fragment.appendChild(makeElement(el, index));
+      resultXHR[index] = author [index];
+    });
+    mapElement.appendChild(fragment);
+    window.card.findPinElements();
+  }
 
-  mapElement.appendChild(fragment);
+
+  window.backend.load(successHandler, window.backend.errorHandler);
+
+  window.pin = {
+    resultXHR: resultXHR
+  };
 
 })();
